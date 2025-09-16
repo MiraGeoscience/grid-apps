f906249e4559ca38b88b899b5cd3ea518c37dfd5
Sébastien Hensgen <24550538+sebhmg@users.noreply.github.com>
2025-09-16 00:27:26 -0400

    [DEVOPS-693] pixi all the way, including for doc
    - fix test for remove the jinja dependency
    - pixi task for build the doc, used in RTD
    - tweak RTD configuration
    - replace install_in_analyst bat by pixi task
    - use pixi to run pylint from pre-commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,8 +8,9 @@
 #                                                                                        '
 # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+import os
 from datetime import datetime
-from importlib.metadata import version
+from importlib.metadata import version as get_version
 
 from packaging.version import Version
 
@@ -26,37 +27,62 @@ project = "grid-apps"
 author = "Mira Geoscience Ltd."
 project_copyright = "%Y, Mira Geoscience Ltd"
 
+package_name = "grid-apps"
+
+full_version = Version(get_version(package_name))
+# The full public version, including alpha/beta/rc tags
+release = full_version.public
+# remove the post release segment, if any
+if full_version.is_postrelease:
+    release = release.rsplit(".post", 1)[0]
+# The short X.Y.Z version.
+version = full_version.base_version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# The full version, including alpha/beta/rc tags.
-release = version("grid-apps")
-# The shorter X.Y.Z version.
-version = Version(release).base_version
-
-autodoc_mock_imports = [
-    "numpy",
-    "geoh5py",
-    "geoapps_utils",
-    "pydantic",
-    "discretize",
-]
+nitpicky = True
 
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx_issues",
+    "sphinxcontrib.googleanalytics",
 ]
+
+intersphinx_mapping = {
+    # use None to auto-fetch objects.inv
+    "numpy": ("https://numpy.org/doc/1.26/", None),
+    "python": ("http://docs.python.org/3", None),
+}
 
 templates_path = ["_templates"]
 exclude_patterns = []
 todo_include_todos = True
 
+googleanalytics_id = os.environ.get("GOOGLE_ANALYTICS_ID", "")
+if not googleanalytics_id:
+    googleanalytics_enabled = False
+
+issues_github_path = f"mirageoscience/{package_name}"
+
 # -- Options for auto-doc ----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#module-sphinx.ext.autodoc
 
 autodoc_typehints = "signature"
+
+autodoc_mock_imports = [
+    "discretize",
+    "geoapps_utils",
+    "geoh5py",
+    "numpy",
+    "pydantic",
+    "scipy",
+    "tqdm",
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output

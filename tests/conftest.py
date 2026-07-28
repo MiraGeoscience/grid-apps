fa10cac7960c5152ca92eaef72c640ea772e6f4c
domfournier <dominiquef@mirageoscience.com>
2026-07-28 14:24:58 -0700

    [GEOPY-2731] Line up setup block model test with develop

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,23 +15,21 @@ from geoh5py.objects import BlockModel
 from grid_apps.block_models.driver import Driver as BlockModelDriver
 
 
-def setup_block_model(
-    workspace,
-    *,
-    top=500,
-    depth_core=300.0,
-    height=300,
-    width=1000,
-    n=100,
-    pads=(100, 150, 200, 300, 0, 0),
-    cell_size=(50, 50, 50),
-) -> BlockModel:
+def setup_block_model(workspace) -> BlockModel:
+    # padding in the W/E/N/S directions should make create locs at least as
+    # far as the core hull plus the padding distances
+    top = 200
+    depth_core = 300.0
+    height = 300
+    width = 1000
+    n = 100
+
     x_grid, y_grid = np.meshgrid(np.arange(0, width, n), np.arange(0, height, n))
     z_grid = np.around((top / 2) * np.sin(x_grid) + (top / 2), -1)
     locs = np.c_[x_grid.ravel(), y_grid.ravel(), z_grid.ravel()]
-
+    pads = [100, 150, 200, 300, 0, 0]
     mesh = BlockModelDriver.get_block_model(
-        workspace, locs, cell_size, depth_core, pads, 1.1, name="test"
+        workspace, locs, [50, 50, 50], depth_core, pads, 1.1, name="test"
     )
     return mesh
 
